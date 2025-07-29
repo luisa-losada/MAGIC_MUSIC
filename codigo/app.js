58f61c4a0a612fffac1b034d0739e2d7062d67fb
luisa-losada <luisa.losada@udea.edu.co>
2025-07-29 11:21:38 -0500

    Se agregó lista de canciones pero falta arreglarlo para que salgan las canciones

--- codigo/app.js
+++ codigo/app.js
@@ -1,11 +1,18 @@
-angular.module("Reproductor", [])
+angular
+  .module("Reproductor", [])
   .controller("ControladorReproductor", function ($scope, $http) {
-    
     $scope.artistas = [];
+    $scope.canciones = [];
 
     //Carga de datos, es solo posible medieante servicios
-    $http.get("datos/Musiteca.json")
-     .then(function(datos) {
-        $scope.artistas=datos.data.Artistas;
-     });
+    $http.get("datos/Musiteca.json").then(function (datos) {
+      $scope.artistas = datos.data.Artistas;
+      console.log($scope.artistas)
+    });
+    $scope.SeleccionarArtista = function (artista) {
+      window.alert(artista.Nombre)
+    }
+    $scope.SelecionarCancion = function(cancion) {
+      window.alert(cancion)
+    }
   });
