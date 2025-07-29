4dffca2fc8de3e6cd50051e8f51c21e90772d638
luisa-losada <luisa.losada@udea.edu.co>
2025-07-29 16:21:11 -0500

    Reproductor finalizado

--- codigo/app.js
+++ codigo/app.js
@@ -3,16 +3,30 @@ angular
   .controller("ControladorReproductor", function ($scope, $http) {
     $scope.artistas = [];
     $scope.canciones = [];
-
+    $scope.artistasSeleccionado = "";
+    $scope.cancionSeleccionada = "";
+    $scope.rutaCancion = "";
+    $scope.reproductor = new Audio();
     //Carga de datos, es solo posible medieante servicios
     $http.get("datos/Musiteca.json").then(function (datos) {
       $scope.artistas = datos.data.Artistas;
-      console.log($scope.artistas)
     });
     $scope.SeleccionarArtista = function (artista) {
-      window.alert(artista.Nombre)
-    }
-    $scope.SelecionarCancion = function(cancion) {
-      window.alert(cancion)
+      $scope.canciones = artista.Canciones;
+      $scope.artistasSeleccionado = artista.Nombre;
+    };
+    $scope.SeleccionarCancion = function (cancion) {
+      $scope.cancionSeleccionada =
+        cancion.Titulo + " - " + $scope.artistasSeleccionado;
+      $scope.rutaCancion =
+        "canciones/" +
+        $scope.artistasSeleccionado +
+        " - " +
+        cancion.Titulo +
+        ".mp3";
+    };
+    $scope.reproducir = function () {
+      $scope.reproductor.src = $scope.rutaCancion;
+      $scope.reproductor.play();
     }
   });
